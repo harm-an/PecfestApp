53297ee7b3e4c1b3cc8e07ddc3443f544e8613ea
Harmanpreet Singh <harman@Harmanpreets-MacBook-Pro.local>
2017-10-23 00:39:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,183 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component, PropTypes} from 'react';
+import {
+  Platform,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Dimensions,
+  Alert,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,
+  ScrollView,
+  AsyncStorage,
+  BackHandler
+
+} from 'react-native';
+
+import {StackNavigator} from 'react-navigation'
+import Page from './Page'
+const colors = { selected: '#ff5a5f', normal: '#484848' };
+const homeIcon = './icons/ic_home_36pt.png'
+import Events from './Events.js'
+
+var loggedIn = 'false';
+var pecfestID = 'null';
+var user = {};
+
+export default class App extends React.Component<{}> {
+  state = {
+    message: "Home",
+    
+  }
+  
+  componentDidMount(){
+    BackHandler.addEventListener('hardwareBackPress', BackHandler.exitApp);
+    AsyncStorage.getItem('loggedIn').then((value) => {
+        if(value == 'true'){
+          AsyncStorage.getItem('user').then((value) => {
+            global.user = JSON.parse(value);
+            global.loggedIn = true;
+          });
+        }
+      });
+  }
+
+  handleClick = message => {
+    if(message == 'Profile'){
+      if(global.loggedIn == true){
+        this.setState({message: 'userDetails'})
+      }else{
+        this.setState({message: message});
+      }
+      
+
+    }else{
+      this.setState({ message: message })
+    }
+  }
+  
+
+  
+  render() {
+  return (
+      <View style={styles.container}>
+        <View style = {styles.pageContainer}>
+          <Page message={this.state.message} />
+        </View>
+        <View style={styles.bottomNav} elevation = {10}>
+          <TouchableNativeFeedback onPress={this.handleClick.bind(this, "Home")} background={TouchableNativeFeedback.Ripple(colors.selected, true)}>
+            <View style = {styles.tabIconContainer} >
+                <Image source = {require('./icons/ic_whatshot.png')}  style={[styles.tabIcon, {tintColor: (this.state.message == 'Home') ? colors.selected : colors.normal}]}/>
+                
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={this.handleClick.bind(this, "Events")} background={TouchableNativeFeedback.Ripple(colors.selected, true)}>
+            <View style = {styles.tabIconContainer} >
+                <Image source = {require('./icons/ic_event.png')} style={[styles.tabIcon, {tintColor: (this.state.message == 'Events') ? colors.selected : colors.normal}]}/>
+                
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={this.handleClick.bind(this, "Team")} background={TouchableNativeFeedback.Ripple(colors.selected, true)}>
+            <View style = {styles.tabIconContainer} >
+                <Image source = {require('./icons/logopf.png')} style={[styles.tabIconMiddle, {tintColor: (this.state.message == 'Team') ? colors.selected : colors.normal}]}/>
+                
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={this.handleClick.bind(this, "Profile")} background={TouchableNativeFeedback.Ripple(colors.selected, true)}>
+            <View style = {styles.tabIconContainer} >
+                <Image source = {require('./icons/ic_person.png')} style={[styles.tabIcon, {tintColor: (this.state.message == 'Profile' || this.state.message == 'userDetails') ? colors.selected : colors.normal}]}/>
+                
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={this.handleClick.bind(this, "Developers")} background={TouchableNativeFeedback.Ripple(colors.selected, true)}>
+            <View style = {styles.tabIconContainer} >
+                <Image source = {require('./icons/code.png')} style={[styles.tabIcon, {tintColor: (this.state.message == 'Developers' ) ? colors.selected : colors.normal}]}/>
+                
+            </View>
+          </TouchableNativeFeedback>
+          
+        </ View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eeeeee',
+  },
+
+  bottomNav: {
+    position:  'absolute',
+    height: 56,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    backgroundColor: "#ffffff",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    
+
+  },
+
+  tabIcon: {
+    
+    height: 24,
+    width: 24,
+  },
+
+  tabIconMiddle: {
+    
+    height: 24,
+    width: 24,
+  },
+
+
+scrollContainer:{
+  position: 'absolute',
+  bottom: 56,
+  top: 0,
+  //height: Dimensions.get('window').height - 56-8,
+},
+ contentContainer: {
+    paddingVertical: 20,
+  }, 
+
+  tabIconContainer: {
+      flex: 1,
+      marginHorizontal: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+  },
+
+  pageContainer: {
+    position: 'absolute',
+    bottom: 56,
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
